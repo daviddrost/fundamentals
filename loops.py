4a01a2d6d0c2be30de4b88a4e9af1ef5470aec63
David Drost <daviddrostcode@gmail.com>
2021-07-13 22:28:38 -0500

    loops core

---
+++ loops.py
@@ -0,0 +1,17 @@
+for i in range(0, 151):
+    print(i)
+
+print(list(range(0, 1000, 5)))
+
+
+N = int(input(500000))
+Sum = 0 
+for i in range(0,n+1):
+   if(i%2!=0):
+      Sum=Sum + i 
+Print(Sum) 
+
+
+
+for i in range(2018, 0, -4):
+    print(i)
